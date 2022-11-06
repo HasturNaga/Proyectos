5763d8f3e3f4a6a7719ba1e8be9c2ed745514b8e
Ricardo <ricardomodon@yahoo.com.ar>
2022-11-06 20:58:41 -0300

    Commit archivos Vehiculo de Python

--- python/LeccionN07/Vehiculo.py
+++ python/LeccionN07/Vehiculo.py
@@ -1,49 +1,36 @@
-class Vehiculo:
-    '''
-    Definir una clase padre llamada Vehiculo y dos clases hijas llamadas Auto y Bicicleta, las cuales heredan de la clase padre Vehiculo. La clase padre debe tener los siguientes atributos y metodos:
-    Vehiculo (clase padre)
-    -Atributos (color, ruedas)
-    -Metodos (__init__(color, ruedas) y __str__())
 
-    Auto (clase hija de Vehiculo)
-    - Atributos (velocidad km/hr)
-    - Metodos (__init__(color, ruedas, velocidad) y __str__())
-
-    Bicicleta (clase hija de Vehiculo)
-    - Atrebutos (tipo(tipo(urbana, montaña, etc.)))
-    - Metodos (__init__(color, ruedas, tipo) y __str__())
-
-    Crear un objeto de cada clase.
-    '''
 class Vehiculo:
 
     def __init__(self, color, ruedas):
-        self._color = color
-        self._ruedas = ruedas
+        self.color = color
+        self.ruedas = ruedas
 
     def __str__(self):
-        return f'Color: {self._color}, Ruedas: {self._ruedas}'
+        return f'Color: {self.color}, Ruedas: {self.ruedas}'
 
 class Auto(Vehiculo):
 
     def __init__(self, color, ruedas, velocidad):
-        super().__init__(self, color, ruedas, velocidad)
-        self._color = color
-        self._ruedas = ruedas
-        self._velocidad = velocidad
+        super().__init__(color, ruedas)
+        self.velocidad = velocidad
 
     def __str__(self):
-        return f'Color: {self._color}, Ruedas: {self._ruedas}'
+        return super().__str__()+' ,Velocidad (km/hr): '+str(self.velocidad)
 
 class Bicicleta(Vehiculo):
 
     def __init__(self, color, ruedas, tipo):
-        super().__init__(self, color, ruedas, tipo)
-        self._color = color
-        self._ruedas = ruedas
-        self._tipo = tipo
+        super().__init__(color, ruedas)
+        self.tipo = tipo
 
     def __str__(self):
-        return f'Color: {self._color}, Ruedas: {self._ruedas}'
+        return super().__str__()+' ,Tipo: '+self.tipo
 
+vehiculo1 = Vehiculo('azul', 3)
+print(vehiculo1)
 
+auto1 = Auto('Verde', 4, 150)
+print(auto1)
+
+bicicleta1 = Bicicleta('Negro', 2, 'Carrera')
+print(bicicleta1)
